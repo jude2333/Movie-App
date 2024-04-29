e1984ad5f3444454ab0144a48bc0d5107dbe92b9
jude2333 <judeshello1991@.com>
2024-04-29 15:24:12 +0530

    first commit

---
+++ AdvancedMovie/app/src/main/java/com/jude/advancedmovie/model/MovieResponse.java
@@ -0,0 +1,57 @@
+package com.jude.advancedmovie.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse {
+
+    @SerializedName("page")
+    @Expose
+    public int page;
+    @SerializedName("results")
+    @Expose
+    public List<Movie> movies;
+
+    @SerializedName("total_results")
+    @Expose
+    public int total_results;
+
+
+    @SerializedName("total_pages")
+    @Expose
+    public int total_pages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+}
